8d6473b190d7c7ffdda0c69a19281465d02830d3
kenji matsuoka <egs@firespeed.org>
2020-06-21 10:39:21 +0900

    Add View model.

--- metronome/src/main/java/org/firespeed/metronome/ui/MetronomeViewModel.kt
+++ metronome/src/main/java/org/firespeed/metronome/ui/MetronomeViewModel.kt
@@ -1,7 +1,23 @@
 package org.firespeed.metronome.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MetronomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val fps: MutableLiveData<Int> by lazy {
+        MutableLiveData<Int>()
+    }
+    val enable: MutableLiveData<Boolean> by lazy {
+        MutableLiveData<Boolean>()
+    }
+
+    fun start() {
+        if (enable.value != true){
+            enable.postValue(true)
+        }
+    }
+
+    fun stop() {
+        enable.postValue(false)
+    }
 }
